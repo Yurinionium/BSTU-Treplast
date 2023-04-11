45ea05d75e944700e8feff0a89eec1b1a29172c0
Yurinionium <em000314@g.bstu.by>
2023-04-11 18:56:38 +0300

    fix: burger

--- js/burger.js
+++ js/burger.js
@@ -3,13 +3,16 @@ const navBar = document.querySelector('.nav');
 
 burger.addEventListener('click', function(e) {
     this.classList.toggle('active');
-    navBar.classList.toggle('active__nav');
-    console.log("Burger is opened");
+    navBar.classList.toggle('active__nav'); 
+    if (navBar.classList.contains('active__nav')) {
+        console.log("Burger is opened");
+        navBar.classList.remove('nav__close');
+        navBar.classList.add('nav__open');
+    } else {
+        console.log("Burger is closed");
+        navBar.classList.remove('nav__open');
+        navBar.classList.add('nav__close');
+    }
 })
 
 
-// calc
-
-// const getElemInput = (input) => {
-//     Number(input.value) * Number(input.dataset.price);
-// }
