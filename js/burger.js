5ca709198d3423ed5b431a98f4d1cf442e22a5ff
Yurinionium <em000314@g.bstu.by>
2023-04-17 18:49:19 +0300

    fix: burger

--- js/burger.js
+++ js/burger.js
@@ -3,15 +3,15 @@ const navBar = document.querySelector('.nav');
 
 burger.addEventListener('click', function(e) {
     this.classList.toggle('active');
-    navBar.classList.toggle('active__nav'); 
-    if (navBar.classList.contains('active__nav')) {
-        console.log("Burger is opened");
-        navBar.classList.remove('nav__close');
-        navBar.classList.add('nav__open');
-    } else {
-        console.log("Burger is closed");
+    // navBar.classList.toggle('active__nav'); 
+    if (navBar.classList.contains('nav__open')) {
+        // console.log("Burger is opened");
+        // navBar.classList.remove('nav__close');
         navBar.classList.remove('nav__open');
-        navBar.classList.add('nav__close');
+    } else {
+        // console.log("Burger is closed");
+        navBar.classList.add('nav__open');
+        // navBar.classList.add('nav__close');
     }
 })
 
